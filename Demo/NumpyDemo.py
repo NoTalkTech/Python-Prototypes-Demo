1447d3494fedaa86658497945c19ef61aff22ae0
huangbiyu <h417652303@163.com>
2017-07-18 18:57:38 +0800

    update NumpyDemo.py

--- Demo/NumpyDemo.py
+++ Demo/NumpyDemo.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats as st
+from scipy import ndimage, misc
 
 if __name__ == '__main__':
     a = np.arange(1, 16).T.reshape((5, 3))
@@ -19,4 +21,17 @@ if __name__ == '__main__':
     count, bins, _ = plt.hist(s, 30, normed=True)
     plt.plot(bins, 1 / (sigma * np.sqrt(2 * np.pi)) * np.exp(- (bins - mu) ** 2 / (2 * sigma ** 2)), linewidth=2,
              color='r')
+    plt.show()
+
+    ascent = misc.ascent()
+    fig = plt.figure()
+    plt.gray()  # show the filtered result in grayscale
+    ax1 = fig.add_subplot(121)  # left side
+    ax2 = fig.add_subplot(122)  # right side
+
+    result = ndimage.gaussian_laplace(ascent, sigma=1)
+    ax1.imshow(result)
+
+    result = ndimage.gaussian_laplace(ascent, sigma=3)
+    ax2.imshow(result)
     plt.show()

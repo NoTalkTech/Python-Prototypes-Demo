9a4ba9058216c9e900c24883fd2bee83935f3b3d
wallace <h417652303@163.com>
2019-11-14 20:04:37 +0800

    [ADD] Add face_recognition_demo.py

--- model/exec_shell_cmd.py
+++ model/exec_shell_cmd.py
@@ -10,14 +10,14 @@ import subprocess
 
 if __name__ == '__main__':
     cmd = ['ls', '-lha']
-    print('---------- call() method ----------')
+    print('\n---------- call() method ----------')
     subprocess.call(cmd, shell=False)
-    print('---------- run() method ----------')
+    print('\n---------- run() method ----------')
     s_proc = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True, shell=False,
                             universal_newlines=True)
     print(s_proc.stdout)
     print('return_code => ', s_proc.returncode)
-    print('---------- Popen() method ----------')
+    print('\n---------- Popen() method ----------')
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
     output, errors = p.communicate()
     print(output)
